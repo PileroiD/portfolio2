4f0cdd59816406760512458a2eb1b51f2596535d
Alex-HP <aleksandr.lozinskiy2005@gmail.com>
2024-05-16 08:11:10 -0400

    webpack config to move images to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: "./src/script.js",
@@ -59,6 +60,9 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: "style.css",
+        }),
+        new CopyWebpackPlugin({
+            patterns: [{ from: "src/images", to: "images" }],
         }),
     ],
     optimization: {
